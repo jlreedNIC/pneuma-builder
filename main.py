ad3cd9fd1d186de909024c494b4fafc0183faad0
Jordan Reed <jlreed@students.nic.edu>
2025-12-12 01:24:38 +0000

    start of pneuma points and update level function

---
+++ main.py
@@ -0,0 +1,19 @@
+# ---------
+# @author   Jordan Reed
+# @date     12/11/25
+# ---------
+
+def update_pneuma_level(points):
+    if pneuma_points == 0:
+        return 1
+
+    temp = pneuma_points / 100
+    """
+    0 - 99 : level 1 : 100 pts
+    100 - 299 : level 2 : 200 pts
+    300 - 500 : level 3 : 300 pts
+
+    level = 
+    """
+pneuma_points = 0
+pneuma_level = 1
