122603aac420f84c64476437241e788766de627b
Jordan Reed <jlreed@students.nic.edu>
2025-12-12 02:30:52 +0000

    fixed algo for update pneuma function. added basic menu and print status function

--- main.py
+++ main.py
@@ -3,11 +3,12 @@
 # @date     12/11/25
 # ---------
 
-def update_pneuma_level(points):
-    if pneuma_points == 0:
-        return 1
+from math import sqrt
 
-    temp = pneuma_points / 100
+def update_pneuma_level(points):
+    level = (-5 + sqrt(25+2*points))/10
+
+    return int(level) + 1
     """
     0 - 99 : level 1 : 100 pts
     100 - 299 : level 2 : 200 pts
@@ -15,5 +16,45 @@ def update_pneuma_level(points):
 
     level = 
     """
+
+def print_status(points, level):
+    print(f'Status')
+    print(f'------')
+    print(f'pneuma points: {points}')
+    print(f'pneuma level: {level}')
+
 pneuma_points = 0
 pneuma_level = 1
+
+print(update_pneuma_level(pneuma_points))
+
+# fake menu
+
+answer = 10
+while(answer != 0):
+    print_status(pneuma_points, pneuma_level)
+
+    print("\nWhat activity did you do today?")
+    print(" (1) Read my bible")
+    print(" (2) Pray")
+    print("\n (0) Exit Game")
+
+    try:
+        answer = int(input("_ "))
+    except Exception as e:
+        print("You put the wrong character in. Try again.")
+        continue
+    
+    if answer == 0:
+        exit(0)
+    elif answer == 1:
+        # read bible
+        pneuma_points += 75
+    elif answer == 2:
+        # pray
+        pneuma_points += 150
+    else:
+        print("That is an incorrect option. Try again.\n")
+    
+    # update
+    pneuma_level = update_pneuma_level(pneuma_points)
