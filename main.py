6c5b421a3fea69c6bd3bfbbfc61a6c3f092489de
Jordan Reed <reed5204@vandals.uidaho.edu>
2025-12-11 19:47:59 -0800

    added activities to menu

--- main.py
+++ main.py
@@ -5,8 +5,11 @@
 
 from math import sqrt
 from pneuma_class import Pneuma
+from activity_class import Activity
 
 my_pneuma = Pneuma()
+act_bible = Activity("Read bible", 50, "01/01/2024")
+act_pray = Activity("Pray", 100, "01/01/2024")
 
 # fake menu
 
@@ -15,8 +18,8 @@ while(answer != 0):
     my_pneuma.print_pneuma()
 
     print("\nWhat activity did you do today?")
-    print(" (1) Read my bible")
-    print(" (2) Pray")
+    print(f" (1) {act_bible.name}")
+    print(f" (2) {act_pray.name}")
     print("\n (0) Exit Game")
 
     try:
@@ -29,10 +32,12 @@ while(answer != 0):
         exit(0)
     elif answer == 1:
         # read bible
-        my_pneuma.add_points(75)
+        my_pneuma.add_points(act_bible.complete_activity())
+        act_bible.print_activity()
     elif answer == 2:
         # pray
-        my_pneuma.add_points(150)
+        my_pneuma.add_points(act_pray.complete_activity())
+        act_pray.print_activity()
     else:
         print("That is an incorrect option. Try again.\n")
     
